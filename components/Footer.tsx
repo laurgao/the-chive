a83a9eaff9c35abb97d2cac98e3da920d9073e07
Laura Gao <lauragao86868@gmail.com>
2021-06-21 16:09:55 -0400

    add github to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,17 @@ export default function Footer() {
     const iconSize = 30;
     return (
         <>
-            <div className="w-full btm-bg-gray-700 text-white px-4 py-16 mt-20 text-center leading-8" style={{backgroundImage: 'url("/bg-dim-50.png")'}}>
+            <div className="w-full btm-bg-gray-700 text-white px-4 py-16 mt-20 text-center leading-10" style={{backgroundImage: 'url("/bg-dim-50.png")'}}>
                 <div className="mx-auto max-w-5xl">
-                    <div className="flex gap-4 justify-center mb-6">
+                    <div className="flex gap-10 justify-center mb-10">
                         <a className="transition theme-hover" href="https://www.youtube.com/channel/UCstSEHcCLMGdac9wkbMeAIw" target="_blank" rel="noreferrer"><FaYoutube size={iconSize}/></a>
                         <a className="transition theme-hover" href="https://twitter.com/laurgao" target="_blank" rel="noreferrer"><FaTwitter size={iconSize}/></a>
                     </div>
                     <p>
-                        Built with ♥ in Toronto by <a href="https://lauragao.ca" className="underline transition theme-hover">Laura Gao</a>
+                        Built with ♥ in Toronto by <a href="https://lauragao.ca" target="_blank" rel="noreferrer" className="underline transition theme-hover">Laura Gao</a>
+                    </p>
+                    <p>
+                        <a className="underline transition theme-hover" href="https://github.com/laurgao/the-chive" target="_blank" rel="noreferrer">Source code</a> on Github
                     </p>
                     <p>
                         The Chive © 2021.
